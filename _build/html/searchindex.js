a158a3d155dd3163bef371e514ad4809c8378375
Marius Gripsgard <mariogrip@ubuntu.com>
2017-03-05 23:45:40 +0100

    Remove _build

--- _build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:50,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Ubports documentation!"],titleterms:{doc:[],document:0,indic:0,tabl:0,ubport:0,welcom:0}})
