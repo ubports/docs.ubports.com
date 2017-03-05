152ecc95b6190130269756ba84381cde1eb6cde6
Marius Gripsgard <mariogrip@ubuntu.com>
2017-03-05 23:36:03 +0100

    Add gitignore and edit theme

--- _build/html/_sources/index.rst.txt
+++ _build/html/_sources/index.rst.txt
@@ -1,9 +1,4 @@
-.. Ubports docs documentation master file, created by
-   sphinx-quickstart on Sun Mar  5 23:09:36 2017.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to Ubports docs's documentation!
+Welcome to Ubports documentation!
 ========================================
 
 .. toctree::
--- conf.py
+++ conf.py
@@ -32,6 +32,14 @@
 # ones.
 extensions = []
 
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -46,8 +54,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Ubports docs'
-copyright = u'2017, Marius Gripsgard'
-author = u'Marius Gripsgard'
+copyright = u'2017, The Ubports project'
+author = u'Ubports developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -82,7 +90,11 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
