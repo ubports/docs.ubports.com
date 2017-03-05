b1e6d7053270121273c11e5b8a02ec918fa8a749
Marius Gripsgard <mariogrip@ubuntu.com>
2017-03-05 23:47:01 +0100

    Set source_suffix on the right place

--- conf.py
+++ conf.py
@@ -38,8 +38,6 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 
-source_suffix = ['.rst', '.md']
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -47,7 +45,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
