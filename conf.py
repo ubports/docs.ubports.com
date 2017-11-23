570d948d7e9b11492e18bc49899ed1650b64e865
Domantas <domantas.mauruca@gmail.com>
2017-11-23 21:54:49 +0100

    Added logo and some styling

--- conf.py
+++ conf.py
@@ -95,6 +95,8 @@ todo_include_todos = True
 import sphinx_rtd_theme
 
 html_theme = "sphinx_rtd_theme"
+html_logo = '_static/images/logo.png'
+
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
@@ -109,7 +111,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
@@ -175,4 +177,4 @@ def setup(app):
             "enable_auto_doc_ref": False,
             }, True)
     app.add_transform(AutoStructify)
-
+    app.add_stylesheet('css/ubports.css')
