eeea67eb6a52f4feeaf9304cbd73d21bc9708e49
Dalton Durst <me@daltondurst.com>
2018-01-02 20:28:14 -0600

    Remove ReCommonMark and add translation support

---
+++ languages.sh
@@ -0,0 +1,12 @@
+# Specify the languages to make po-files for, one per line.
+# There are many languages for which the docs interface will be translated,
+# Listed here: http://www.sphinx-doc.org/en/1.5.1/config.html#confval-language
+# If the desired language is not listed there, simply add its ISO code.
+LANGUAGES=" 
+tr
+fr
+de
+es
+it
+ro
+"
---
+++ update-translations.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source languages.sh
+
+# color codes
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+PLAIN='\033[0m'
+
+# check if virtualenv already exists
+if [ -d "$HOME/ubportsdocsenv" ]; then
+  echo -e "${GREEN}Build environment found.${PLAIN}"
+  . ~/ubportsdocsenv/bin/activate
+else
+  echo -e "${RED}No build environment found.${PLAIN}"
+  echo -e "${YELLOW}Installing pip and virtualenv.${PLAIN}"
+  sudo apt install python-pip
+  sudo -H pip install virtualenv
+  echo -e "${YELLOW}Creating a virtual environment in ${HOME}/ubportsdocsenv.${PLAIN}"
+  virtualenv ~/ubportsdocsenv
+  . ~/ubportsdocsenv/bin/activate
+  echo -e "${YELLOW}Installing build tools and prerequisites.${PLAIN}"
+  sudo -H pip install sphinx sphinx_rtd_theme sphinx-intl
+fi
+echo -e "${GREEN}Building...${PLAIN}"
+sphinx-build -Wa . _build/gettext -b gettext -j `nproc --all`
+
+echo -e "${GREEN}Building po-files${PLAIN}"
+for LANGUAGE in $LANGUAGES; do
+    sphinx-intl update -l $LANGUAGE
+done
